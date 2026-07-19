506cd97f4e592fe08e2d8d40590523d62ba413dd
heartfulnessgujarat <heartfulness.gujarat@gmail.com>
2026-07-20 01:02:09 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@ const CONFIG = {
 
     APP_NAME: "RSHM",
 
-    API_URL: "PASTE YOUR APPS SCRIPT WEB APP URL HERE"
+    API_URL: "https://script.google.com/macros/s/AKfycbwAEs0e3tRG2aq0kZjPWPoUZkh2Q3Q-YJf--xSxxb97bvF-hCI-j_qlG7NBL3MHGJk-/exec"
 
 };
