8a31a5c021daacd15c5af7e2974d8928c093a0e2
heartfulnessgujarat <heartfulness.gujarat@gmail.com>
2026-07-20 23:36:45 +0530

    Delete js directory

--- js/app.js
+++
@@ -1,3 +0,0 @@
-
-console.log("RSHM App Loaded");
-
--- js/config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-
-    APP_NAME: "RSHM",
-
-    API_URL: "https://script.google.com/macros/s/AKfycbwAEs0e3tRG2aq0kZjPWPoUZkh2Q3Q-YJf--xSxxb97bvF-hCI-j_qlG7NBL3MHGJk-/exec"
-
-};
